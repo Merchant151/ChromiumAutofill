4a7b4541f6a992a695faa8b8513b4f9ff7801aa8
Merchant151 <joshntram@gmail.com>
2025-07-22 20:43:11 -0400

    picked template page and added psudo plan to new js script

---
+++ basic.js
@@ -0,0 +1,8 @@
+console.log('Hello, script is active on fourm pages.');
+
+
+//Psudo stuff
+//fill dictionary
+//for fourm item on page
+//identify form
+//if fourm identity in dictionary fill fourm with result.
