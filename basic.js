cdd7663ddfe2f691756df20ecf40710036bdad60
Merchant151 <joshntram@gmail.com>
2025-07-23 23:58:02 -0400

    working on query selector for input

--- basic.js
+++ basic.js
@@ -3,6 +3,12 @@ console.log('Hello, script is active on fourm pages.');
 
 //Psudo stuff
 //fill dictionary
+	//login stuff needs to exist separately or not at all
 //for fourm item on page
+const elements = document.querySelectorAll("input");
+elements.forEach(function(){
+	console.log('found element')
+
+});
 //identify form
 //if fourm identity in dictionary fill fourm with result.
