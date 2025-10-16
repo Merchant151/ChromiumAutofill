82e47ead18a11380c8c95ee8cdc50ac8962701d0
Merchant151 <joshntram@gmail.com>
2025-10-16 09:05:53 -0400

    subscription worker issues

--- worker.js
+++ worker.js
@@ -1,9 +1,10 @@
 //service worker (for debug actions)
 
-chrome.runtime.onMessage.addListener((message,sender,sendResponse)
+console.log('service worker is registered and active!');
+chrome.runtime.onMessage.addListener((message,sender,sendResponse),
 	if (message.type === 'test'){
 		console.log("service worker test successful",message.data);
 		sendResponse({status: "complete"});
 	}
 
-)
+);
