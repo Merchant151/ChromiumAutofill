905eeac3af181ba0891bc8f76eccbf64e826faac
Merchant151 <joshntram@gmail.com>
2026-03-15 23:48:10 -0400

    get label improvements

--- basic.js
+++ basic.js
@@ -563,6 +563,10 @@ function getInputLabel(qElm){
 		//child is legend l span
 		let llspan = elm.closest('fieldset').querySelector('legend label span');
 		return llspan.textContent;
+	}else if (elm.hasAttribute('type')&&elm.getAttribute('type') === 'button'){
+		//TODO:: IF qType drop down get previous question same method as above parent and then how did. 
+		//MAYBE add as a condition to the previous if don't rewrite code! 
+		return 'buttonType'
 	} 
 	return 'unknown!';
 
